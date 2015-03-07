b57cc98e9b458399b81e02dabf61e96f934ba0ee
Josh Woodward <josh_woodward@medisked.com>
2015-03-07 18:43:20 -0500

    No content validation error

--- spec/features/todo_items/create_spec.rb
+++ spec/features/todo_items/create_spec.rb
@@ -15,7 +15,7 @@ describe 'Adding todo items' do
 
 	it 'is successful with valid content' do
 		visit_todo_list(todo_list)
-		click_link("New Todo Item")
+		click_link "New Todo Item"
 		fill_in "Content", with: "Milk"
 		click_button "Save"
 
@@ -23,6 +23,17 @@ describe 'Adding todo items' do
 		within("ul.todo_items") do
 			expect(page).to have_content("Milk")
 		end
+	end 
 
+	it 'displays an error with no content' do
+		visit_todo_list(todo_list)
+		click_link "New Todo Item"
+		fill_in "Content", with: ""
+		click_button "Save"
+
+		within("div.flash") do
+			expect(page).to have_content("There was a problem adding that todo list item.")
+		end 
+		expect(page).to have_content("Content can't be blank")
 	end 
 end 
