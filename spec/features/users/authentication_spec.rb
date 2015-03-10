10577a31a874a89f67d728c7d33e32295735e264
Josh Woodward <josh_woodward@medisked.com>
2015-03-09 23:33:21 -0400

    Additional Authentication tests

--- spec/features/users/authentication_spec.rb
+++ spec/features/users/authentication_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe 'loggin in' do 
 
+	it 'logs the user in and goes to the todo lists' do
+		User.create(first_name: "User", last_name: "Test", email: "user@user.com", password: "password", password_confirmation: "password")
+		visit new_user_session_path
+		fill_in "Email Address", with: "user@user.com"
+		fill_in "Password", with: "password" 
+		click_button "Log In"
+		expect(page).to have_content("Todo Lists")
+		expect(page).to have_content("Thanks for logging in!")
+	end 
+
 	it 'displays the email address in the event of a failed login' do
 		visit new_user_session_path
-		fill_in "Email", with: "user@user.com"
+		fill_in "Email Address", with: "user@user.com"
 		fill_in "Password", with: "incorrect"
 		click_button "Log In"
 
