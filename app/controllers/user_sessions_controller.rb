7a1a90b7947e45ae166f32178202d247fb16f71a
Josh Woodward <josh_woodward@medisked.com>
2015-03-09 22:42:27 -0400

    Enhanced user session create to meet spec

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -6,10 +6,15 @@ class UserSessionsController < ApplicationController
   def create
 
   	user=User.find_by(email: params[:email])
-  	user.authenticate(params[:password])
+  	
+  	if user && user.authenticate(params[:password])
+	  	session[:user_id]=user.id
+	  	flash[:success]="Thanks for logging in!"
+	  	redirect_to todo_lists_path
+	else
+		flash[:error]="There was a problem logging in. Please check your email and password"
+		render action: 'new'
+	end
 
-  	session[:user_id]=user.id
-
-  	redirect_to todo_lists_path
   end
 end
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -43,9 +43,46 @@ describe UserSessionsController do
         post :create, email: "user@user.com", password: "password"
         expect(session[:user_id]).to eq(user.id)
 
+      end 
 
+      it 'sets the flash success message' do
+          post :create, email: 'user@user.com', password: 'password'
+          expect(flash[:success]).to eq("Thanks for logging in!")
       end 
 
     end
+
+    shared_examples_for "denied login" do
+      it 'renders the new template' do
+        post :create, email: email, password: password
+        expect(response).to render_template('new')
+      end 
+
+      it 'sets the flash error message' do
+        post :create, email: email, password: password
+        expect(flash[:error]).to eq("There was a problem logging in. Please check your email and password")
+      end
+    end 
+
+
+    context 'with blank credentials' do
+      let(:email){""}
+      let(:password){""}
+      it_behaves_like 'denied login'
+    end 
+  
+    context 'with an incorrect password' do
+      let!(:user){User.create(first_name: "User", last_name: "Test", email: "user@user.com", password: "password", password_confirmation: "password")}  
+      let(:email){user.email}
+      let(:password){"wrongpassword"}
+      it_behaves_like 'denied login'
+    end 
+
+    context 'with no email in existence' do
+      let(:email){"none@found.com"}
+      let(:password){"incorrect password"}
+      it_behaves_like "denied login"
+    end
+
   end
 end
