0058bc88799b525e558aa9886f413f3f21be7196
Josh Woodward <josh_woodward@medisked.com>
2015-03-10 21:15:19 -0400

    Added password reset migration, test, and update reset token function

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,4 +63,19 @@ describe User do
 		end	
 
 	end
+
+	describe "#generate_password_reset_token!" do
+		let(:user){create(:user)}
+
+		it 'changes the password_reset_token attribute' do
+			expect{user.generate_password_reset_token!}.to change{user.password_reset_token}
+		end 
+	
+		it 'calls SecureRandom.urlsafe_base64 to generate the password_reset_token' do
+			expect(SecureRandom.to receive(:urlsafe_base64(48)
+		end 
+
+	end 
+
+
 end
