d3297d2e09aca1adacdac1a49ce4c6d0c453dd9a
Josh Woodward <josh_woodward@medisked.com>
2015-03-15 22:50:21 -0400

    Todo_Lists endpoint working with index method

---
+++ app/controllers/api/todo_lists_controller.rb
@@ -0,0 +1,7 @@
+class Api::TodoListsController < ApplicationController
+
+	def index
+		render json: TodoList.all
+	end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :api do
+    resources :todo_lists
+  end 
+
   root 'todo_lists#index'
 
 
